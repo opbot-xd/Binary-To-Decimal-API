41302fe9913acbd69becadd050a6ebaf84b5a4d9
Krishna Awasthi <awasthikrishna23052005@gmail.com>
2024-07-11 12:51:01 +0530

    feat: fixed api

--- app.py
+++ app.py
@@ -7,28 +7,22 @@ def hello_world():
 
 @app.route('/dtb', methods=["POST"])
 def decimalToBinary():
-     input_json = request.get_json(force=True)
-     outputNumber = bin(int(input_json['num'])).replace("0b", "")
-     dictToReturn = {'output':outputNumber}
-     return jsonify(dictToReturn)   
-
-@app.route('/posting', methods=["POST"])
-def testpost():
-     input_json = request.get_json(force=True) 
-     dictToReturn = {'text':input_json['text']}
-     return jsonify(dictToReturn)
+    input_json = request.get_json(force=True)
+    outputNumber = bin(int(input_json['num'])).replace("0b", "")
+    dictToReturn = {'output':outputNumber}
+    return jsonify(dictToReturn)   
 
 
 @app.route('/btd', methods=["POST"])
 def binaryToDecimal():
-     input_json = request.get_json(force=True)
-     if (input_json['num'])[0]!='-':
+    input_json = request.get_json(force=True)
+    if (input_json['num'])[0]!='-':
         outputNumber = int('0b'+input_json['num'],2)
-     else:
-         outputNumber = int('0b'+(input_json['num'])[1:],2)*-1
+    else:
+        outputNumber = int('0b'+(input_json['num'])[1:],2)*-1
 
-     dictToReturn = {'output':str(outputNumber)}
-     return jsonify(dictToReturn)
+    dictToReturn = {'output':str(outputNumber)}
+    return jsonify(dictToReturn)
 
 if __name__=="__main__":
     app.run()
